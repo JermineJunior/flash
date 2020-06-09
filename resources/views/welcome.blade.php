fe9be1b0b258556d419f80399eb35142209afe28
Mohammed Ibrahim <obietrice734@gmail.com>
2020-06-09 21:17:43 +0200

    =update content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="w-full md:w-1/2 pt-24">
         <div class=" text-gray-800 px-6 tracking-wide">
             <h1 class="lg:text-4xl sm:text-lg my-4 italic">Welcome to My <i class="text-orange-500">Ecommerce</i> website , have fun.</h1>
-            <a class="text-xl inline-block no-underline  leading-relaxed" href="#">Browse products</a>
+            <a class="text-xl inline-block no-underline  leading-relaxed" href="#">Browse All Books.</a>
         </div>
     </div>
     <div class="w-full md:w-1/2 pt-4">
